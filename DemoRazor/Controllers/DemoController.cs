4c8bcdda8d880e524c89e88022fc0bf4d175d573
PHAM DUC MANH <manh181202862@st.utc.edu.vn>
2021-02-26 00:11:35 +0700

    razor commit

---
+++ DemoRazor/Controllers/DemoController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoRazor.Models;
+
+namespace DemoRazor.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+         [HttpPost]
+        public ActionResult Index(FormCollection f)
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Demo()
+        {
+            List<ThanhPho> lsTP = new List<ThanhPho>();
+            ThanhPho tp;
+            tp = new ThanhPho();
+            tp.MaTP = 1;
+            tp.TenTP = "Hà Nội";
+            lsTP.Add(tp);
+            tp = new ThanhPho();
+            tp.MaTP = 2;
+            tp.TenTP = "TP.Hồ Chí Minh";
+            lsTP.Add(tp);
+
+            //ViewBag dữ liệu động dùng để add dữ liệu
+            ViewBag.tennaocungduoc = new SelectList(lsTP, "MaTP", "TenTP");
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Demo(FormCollection f)
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ DemoRazor/Models/ThanhPho.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoRazor.Models
+{
+    public class ThanhPho
+    {
+        public int MaTP
+        {
+            get;
+            set;
+
+        }
+        public string TenTP
+        {
+            get;
+            set;
+
+        }
+    }
+}
