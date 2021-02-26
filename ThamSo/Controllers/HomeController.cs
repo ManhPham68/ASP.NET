c11a5f49b114a747e58b3d3df2260ca31c7c965b
Your Name <you@example.com>
2021-02-26 20:00:04 +0700

    thamso commit

---
+++ ThamSo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThamSo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Hello(String id)
+        {
+            ViewBag.HoTen = id;
+            return View();
+        }
+
+
+    }
+}
---
+++ ThamSo/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThamSo.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ThongTinTaiKhoan(FormCollection f , string HoTen)
+        {
+            ViewBag.HoTen = HoTen;
+            ViewBag.TaiKhoan = f.Get("txtTenDangNhap").ToString();
+            return View();
+        }
+    }
+}
